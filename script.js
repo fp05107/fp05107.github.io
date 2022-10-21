36780c17b069e31f5bbdad506b714669e8e8365b
subham <andharalauri@gmail.com>
2022-10-21 14:38:50 +0530

    added

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
     
 }
+
+
+// -------------------responsiveness----------------
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+
