43a35314819e188777827f682a59f43336fe8af8
subham <andharalauri@gmail.com>
2022-10-22 22:57:45 +0530

    added

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// var tabLinks = document.getElementsByClassName("tab-links");
-// var tabcontents = document.getElementsByClassName("tab-contents");
-// function opentab(tabname){
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
 
-//     for(tabLink of tabLinks){
-//         tabLink.classList.remove("active-link");
-//     }
-//     for(tabcontent of tabcontents){
-//         tabcontent.classList.remove("active-tab");
-//     }
-//     event.currentTarget.classList.add("active-link");
-//     document.getElementById(tabname).classList.add("active-tab");
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
     
-// }
+}
 
 
 // -------------------responsiveness----------------
