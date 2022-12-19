6df77eda6a50e0b7fc71c97dfd4a4a44508742ce
subham <andharalauri@gmail.com>
2022-12-19 14:46:43 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+var resume = document.getElementById("resume");
+resume.addEventListener('click',function(){
+    window.open("https://drive.google.com/file/d/1AOHf46vMzZFKLEHMTg1d9dF4BrnIjwyN/view?usp=sharing")
+})
+
+//Project Section//
+let olx_git = document.getElementById("olx_git");
+let olx_live = document.getElementById("olx_live");
+
+olx_git.addEventListener('click',function(){
+    window.open("https://github.com/tarique076/OLX_Clone")
+})
+olx_live.addEventListener('click',function(){
+    window.open("https://voluble-bombolone-fdb49d.netlify.app/")
+})
+
+
+let dermstore_git = document.getElementById("dermstore_git");
+let dermstore_live = document.getElementById("dermstore_live");
+
+dermstore_git.addEventListener('click',function(){
+    window.open("https://github.com/fp05107/Dermstore_clone")
+})
+dermstore_live.addEventListener('click',function(){
+    window.open("https://extraordinary-sunburst-4c6b00.netlify.app")
+})
+
+let bus_git = document.getElementById("bus_git");
+let bus_video = document.getElementById("bus_video");
+
+bus_git.addEventListener('click',function(){
+    window.open("https://github.com/fp05107/long-dinner-956")
+})
+bus_video.addEventListener('click',function(){
+    window.open("https://drive.google.com/file/d/1snpKcv4vYFDvXvgmWf-AfCJPkAyY_xFy/view?usp=share_link")
+})
+
+let auction_git = document.getElementById("auction_git");
+let auction_video = document.getElementById("auction_video");
+
+auction_git.addEventListener('click',function(){
+    window.open("https://github.com/fp05107/Automated_Auction_System")
+})
+auction_video.addEventListener('click',function(){
+    window.open("https://drive.google.com/file/d/1bkpx7RgiOFbGZu23wwQDbP66Qgciu3BL/view?usp=sharing")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Project Section End
+
+
 function opentab(tabname) {
 
     for (tabLink of tabLinks) {
